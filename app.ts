409e270ee73391e840d397a350b5d44db3783fd2
Max Axenov <maksenov@wavesplatform.com>
2023-03-22 23:56:50 +0300

    feat: users stuff

--- app.ts
+++ app.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import morgan from 'morgan';
+import { userRouter } from './routes/userRouter';
 
-import { referralRouter } from './routes/referralRouter';
 
 export const app = express();
 
@@ -10,12 +10,8 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 app.use(express.json());
+
 app.use(express.static(`${__dirname}/public`));
 
-app.use((req, res, next) => {
-  console.log('Hello from the middleware 👋');
-  next();
-});
 
-
-app.use('/api/v1/referrals', referralRouter);
+app.use('/api/v1/users', userRouter);
---
+++ controllers/userController.ts
@@ -0,0 +1,61 @@
+import { User } from '../models/userModel';
+
+export const getAllUsers = async (req, res) => {
+    const users = await User.find();
+    try {
+        res.status(200).json({
+            status: 'success',
+            results: users.length,
+            data: {
+                users
+            },
+        });
+    } catch (err) {
+        res.status(404).json({
+            status: 'fail',
+            message: err,
+        });
+    }
+};
+
+export const createUser = async (req, res) => {
+    try {
+        console.log(req.body)
+        const newUser = await User.create(req.body);
+
+        res.status(201).json({
+            status: 'success',
+            data: {
+                user: newUser,
+            },
+        });
+    } catch (err) {
+        console.log(err)
+        res.status(400).json({
+            status: 'fail',
+            message: err,
+        });
+    }
+};
+
+export const addReferral = async (req, res) => {
+    try {
+        await User.findOneAndUpdate(
+            { referralId: req.params.id },
+            { $push: { referrals: req.body } },
+            {
+                new: true,
+                runValidators: true,
+            }
+        );
+
+        res.status(200).json({
+            status: 'success'
+        });
+    } catch (err) {
+        res.status(400).json({
+            status: 'fail',
+            message: err,
+        });
+    }
+};

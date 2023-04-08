f77c69031bdeb78b38f02660222dcc214f71565d
Max Axenov <maksenov@wavesplatform.com>
2023-04-09 01:02:19 +0300

    fix port

--- server.ts
+++ server.ts
@@ -15,7 +15,7 @@ mongoose
 
     console.log(process.env.NODE_ENV)
     console.log(process.env.PORT)
-const port = process.env.NODE_ENV === 'development' ? (process.env.PORT || 80) : 4000;
+const port = 4000;
 
 app.listen(port, () => {
     console.log(`App running on port ${port}...`);

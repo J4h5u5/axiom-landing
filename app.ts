0d0e08aba0840836a5a41c7b6b84b8d0f72fa28c
Max Axenov <maksenov@wavesplatform.com>
2023-04-09 00:43:47 +0300

    try fix

--- app.ts
+++ app.ts
@@ -21,7 +21,7 @@ if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
 
-app.use(morgan('tiny', { stream: fs.createWriteStream('./server.log', { flags: 'a' }) }));
+// app.use(morgan('tiny', { stream: fs.createWriteStream('./server.log', { flags: 'a' }) }));
 
 app.use(express.json());
 
@@ -36,32 +36,32 @@ readFile(`${__dirname}/milesConfig.json`, 'utf8').then((data) => {
     milesConfig = JSON.parse(data);
 });
 
-// cron.schedule('0 0 0 * * *', function () {
-//     const stream = fs.createWriteStream('./miles.log', { flags: 'a' });
+cron.schedule('0 0 0 * * *', function () {
+    const stream = fs.createWriteStream('./miles.log', { flags: 'a' });
 
-//     stream.write(`${Date.now()}:\n`);
+    stream.write(`${Date.now()}:\n`);
 
-//     User.updateMany(
-//         {},
-//         {
-//             $set: { lastDailyMilesPayout: new Date() },
-//             $inc: { miles: milesConfig.daily },
-//         }
-//     )
-//         .then((a) => {
-//             stream.write('daily miless paid succesfull');
-//             stream.end();
-//         })
-//         .catch((err) => {
-//             stream.write(`daily miless did not paid, error:\n${err}`);
-//             stream.end();
-//         });
-// });
+    User.updateMany(
+        {},
+        {
+            $set: { lastDailyMilesPayout: new Date() },
+            $inc: { miles: milesConfig.daily },
+        }
+    )
+        .then((a) => {
+            stream.write('daily miless paid succesfull');
+            stream.end();
+        })
+        .catch((err) => {
+            stream.write(`daily miless did not paid, error:\n${err}`);
+            stream.end();
+        });
+});
 
-// cron.schedule('0 0 * * 0', function () {
-//     // каждую неделю чистить server.log
-//     fs.writeFile(`${__dirname}/server.log`, `Cleared at: ${new Date().toString()}`, (err) => {
-//         if (err) throw err;
-//         console.log('The log file was cleared!');
-//     });
-// });
+cron.schedule('0 0 * * 0', function () {
+    // каждую неделю чистить server.log
+    fs.writeFile(`${__dirname}/server.log`, `Cleared at: ${new Date().toString()}`, (err) => {
+        if (err) throw err;
+        console.log('The log file was cleared!');
+    });
+});
